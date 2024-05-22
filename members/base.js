ae4c5d5b3997432abc497606cb434c730f97f408
GreyAlien502 <greyalien502@gmail.com>
2024-05-22 22:17:12 +0000

    improve security

--- members/base.js
+++ members/base.js
@@ -1,7 +1,7 @@
 let API = (command,data) => {
 	let encode = str => encodeURIComponent(str).replace(/[!'()*]/g, escape);
 	return fetch(
-		"http://24.45.22.117:7265/memebers.js?"+
+		"https://mebio"+"us.uk.to/memebers.js?"+
 		(queries=>Object.keys(queries).map( key => encode(key)+'='+encode(queries[key]) ))({
 			'username':localStorage.getItem("username"),
 			'password':localStorage.getItem("password"),
